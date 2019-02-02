72e72d80b3edceefcf4fd2157a47de1cdab56d24
Eric C Morgan <morganer@oregonstate.edu>
2019-02-02 23:27:24 +0100

    Created docker for testing and started auth.

--- webapp/src/Main.js
+++ webapp/src/Main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (<div className="card mt-3">
